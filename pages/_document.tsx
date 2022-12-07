6f85ffbba1ba599015c3f5f387fa388fbd84c3d4
Srikar Samudrala <srikarsams@gmail.com>
2022-12-07 11:06:37 +0530

    Fixes syntax error

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -22,7 +22,7 @@ class MyDocument extends Document {
           <script
             async
             src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8059115849794120"
-            crossorigin="anonymous"
+            crossOrigin="anonymous"
           ></script>
         </Head>
         <body>
